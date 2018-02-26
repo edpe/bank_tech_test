1687537b400eb4f68dcf92e695a42b632fa11597
Ed Perkins <ed@edperkins.co.uk>
2018-02-26 14:25:51 +0000

    add deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,15 @@
 class Account
 
-
-
   def initialize(initial_balance = 0.00)
     @balance = initial_balance
   end
 
   def balance
     @balance
+  end
+
+  def deposit(amount)
+    @balance += amount
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,12 @@ describe Account do
     expect(account.balance).to eq 0
   end
 
+  describe '#deposit' do
+    it "adds the deposited amount to the balance" do
+      expect { account.deposit 100.00 }.to change { account.balance }.by 100.00
+    end
+  end
+
+
+
 end
